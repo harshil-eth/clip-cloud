88c484752664369ea755e0308037d2656a899247
21-kunal <onlyforcc7@gmail.com>
2024-06-09 19:44:39 +0530

    Add Chrome extension context menu item
    - Removed unnecessary files and code.
    - Modified manifest.json to define extension metadata.
    - Added background.js to handle background tasks.
    - Implemented context menu item in Chrome extension.

---
+++ extensions/clip-cloud/public/background.js
@@ -0,0 +1,34 @@
+chrome.contextMenus.onClicked.addListener(genericOnClick);
+
+function genericOnClick(info) {
+  if (info.menuItemId === "radio") {
+    console.log("Radio item clicked. Status:", info.checked);
+  } else if (info.menuItemId === "checkbox") {
+    console.log("Checkbox item clicked. Status:", info.checked);
+  } else {
+    console.log("Standard context menu item clicked.");
+  }
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+  let contexts = [
+    "page",
+    "selection",
+    "link",
+    "editable",
+    "image",
+    "video",
+    "audio",
+  ];
+
+  for (let i = 0; i < contexts.length; i++) {
+    let context = contexts[i];
+    let title = "Save " + context;
+
+    chrome.contextMenus.create({
+      title: title,
+      contexts: [context],
+      id: context,
+    });
+  }
+});
