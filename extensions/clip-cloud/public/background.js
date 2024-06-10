453396f4948d5327104cf1c84d76eabd258a51d2
whoiskunal <kunal.prajapati@hertshtengroup.com>
2024-06-10 13:35:10 +0100

    Add Prettier for code formatting and reformat all files
    - Install and configure Prettier for consistent code formatting.
    - Reformat all existing files to adhere to the Prettier style guide.
    - Update .prettierrc or any relevant configuration files.
    - Ensure no functional code changes, only formatting adjustments.

--- extensions/clip-cloud/public/background.js
+++ extensions/clip-cloud/public/background.js
@@ -1,29 +1,29 @@
 chrome.contextMenus.onClicked.addListener(genericOnClick);
 
 function genericOnClick(info) {
-  if (info.menuItemId === "radio") {
-    console.log("Radio item clicked. Status:", info.checked);
-  } else if (info.menuItemId === "checkbox") {
-    console.log("Checkbox item clicked. Status:", info.checked);
+  if (info.menuItemId === 'radio') {
+    console.log('Radio item clicked. Status:', info.checked);
+  } else if (info.menuItemId === 'checkbox') {
+    console.log('Checkbox item clicked. Status:', info.checked);
   } else {
-    console.log("Standard context menu item clicked.");
+    console.log('Standard context menu item clicked.');
   }
 }
 
 chrome.runtime.onInstalled.addListener(function () {
   let contexts = [
-    "page",
-    "selection",
-    "link",
-    "editable",
-    "image",
-    "video",
-    "audio",
+    'page',
+    'selection',
+    'link',
+    'editable',
+    'image',
+    'video',
+    'audio',
   ];
 
   for (let i = 0; i < contexts.length; i++) {
     let context = contexts[i];
-    let title = "Save " + context;
+    let title = 'Save ' + context;
 
     chrome.contextMenus.create({
       title: title,
